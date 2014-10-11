e3f8ef72d014a7dd890cae83a0d001dca83de2a6
Alina Solovjova <alina@gocardless.com>
2014-10-11 14:37:15 +0100

    Initialise repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'e2fc4cb9456f56ad911962e0ca09c6fb63445525916daa9a5c9a9acfb42cd2b93c6b2d4dcb0902c056d3b5cd54652ee4a2457292181de58334c45ab4b3b4b041'
